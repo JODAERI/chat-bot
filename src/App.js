eb02fb04e30145902b254428ceacf7582c340931
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-05 11:56:02 +0900

    setting: chat 페이지

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import MainPage from "./pages/main";
+import styled from "styled-components";
+import Chat from "./pages/chat";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path={'/'} element={<MainPage/>}/>
-      </Routes>
-    </>
+    <Wrapper>
+        <Container>
+            <Routes>
+                <Route path={'/'} element={<MainPage/>}/>
+                <Route path={'/chat'} element={<Chat/>}/>
+            </Routes>
+        </Container>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+    overflow: hidden;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: ${({theme}) => theme.backgroundColor.background};
+`
+
+const Container = styled.div`
+    width: ${({theme}) => theme.tablet};;
+    background: white;
+    height: 100vh;
+    overflow-y: scroll;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    @media (max-width:${({theme}) => theme.mobile} ) {
+        width: 100vw;
+        height: 100vh;
+    }
+
+`
