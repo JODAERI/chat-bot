521974b7d24091619643e41f4a2df6bb2bcca65e
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-07 00:00:19 +0900

    feat: 헤더, 인풋 추가

---
+++ src/components/common/Header.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+function Header() {
+    return (
+        <>
+            <LogoBox>
+                <Link style={{ textDecoration: "none"}} to={'/'}>
+                    <LogoItem>
+                        조대리
+                    </LogoItem>
+                </Link>
+            </LogoBox>
+        </>
+
+    );
+}
+export default Header;
+
+const LogoBox = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 20px;
+    padding-bottom: 30px;
+ 
+    
+`
+const LogoItem = styled.div`
+    @font-face {
+        font-family: 'RiaSans-ExtraBold';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2410-1@1.0/RiaSans-ExtraBold.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+    font-family: 'RiaSans-ExtraBold';
+    font-size: 40px;
+    color: ${({theme}) => theme.colors.logoFont};
+    pointer-events: visibleStroke;
+    &:hover{
+        color: ${({theme}) => theme.colors.btnFont};
+    }
+`
---
+++ src/components/common/Input.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import {SendIcon} from "./icons";
+
+
+function Input() {
+    return (
+        <>
+            <Wrapper>
+                <InputBox>
+                    <InputItem placeholder={'메세지를 입력해 주세요.'}/>
+                    <IconBox>
+                        <SendIcon/>
+                    </IconBox>
+                </InputBox>
+            </Wrapper>
+        </>
+
+    );
+}
+
+export default Input;
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 45px;
+    
+`
+const InputBox = styled.div`
+    border-radius: 10px;
+    border: solid 1px ${({theme})=>theme.colors.inputBorder};
+    height: 57px;
+    background:  ${({theme})=>theme.backgroundColor.inputBack};
+    width: 80%;
+    display: flex;
+    text-align: center;
+    @media (max-width:${({theme}) => theme.mobile} ) {
+        width: 390px;
+    }
+
+`
+
+const IconBox = styled.div`
+    margin: auto;
+    display: flex;
+    text-align: center;
+    justify-content: flex-end;
+  
+`
+
+const InputItem = styled.textarea`
+    border-radius: 10px;
+    justify-content: flex-start;
+    border: none;
+    outline: none;
+    resize: none;
+    padding-top: 20px;
+    padding-left: 10px;
+    font-size: 15px;
+    width: 85%;
+    @media (max-width:${({theme}) => theme.mobile} ) {
+        width: 80%;
+    }
+`
