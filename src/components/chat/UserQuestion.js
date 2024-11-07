0e5e79274b6d67bc768d00fe0565c62072326fb3
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-08 01:05:44 +0900

    css: 사용자 질문창 UI

--- src/components/chat/UserQuestion.js
+++ src/components/chat/UserQuestion.js
@@ -1,9 +1,54 @@
-function UserQusetion({question, timeStamp}) {
+import styled from "styled-components";
+
+function UserQuestion({question, timeStamp}) {
     return (
         <>
+            <Wrapper>
+                <ContentsBox>
+                    <TimeStampBox>
+                        {timeStamp}
+                    </TimeStampBox>
+                    <AnswerBox>
+                        {question}
+                    </AnswerBox>
+                </ContentsBox>
+            </Wrapper>
         </>
 
     );
 }
 
-export default UserQusetion;
+export default UserQuestion;
+
+const Wrapper = styled.div`
+    display: flex;
+    text-align: center;
+    justify-content: flex-end;
+    width: 80%;
+    margin: auto;
+    @media (max-width:${({theme}) => theme.mobile} ) {
+        width: 95%;
+    }
+`
+
+const ContentsBox = styled.div`
+`
+
+const TimeStampBox = styled.div`
+    display: flex;
+    text-align: center;
+    justify-content: flex-start;
+    color:${({theme})=>theme.colors.timeStamp};
+    padding: 10px;
+`
+
+const AnswerBox = styled.div`
+    background: ${({theme})=>theme.backgroundColor.userQuestion};
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    padding: 20px 10px;
+    @media (max-width:${({theme}) => theme.mobile} ) {
+        margin: 0 10px ;
+    }
+`
