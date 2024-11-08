2a2df58019b94868d364fdd1e7044324cbab6f4e
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-09 00:51:06 +0900

    docs: 메인페이지 코드 분리

---
+++ src/components/main/MainView.js
@@ -0,0 +1,81 @@
+import {Link} from "react-router-dom";
+import {BtnIcons} from "./BtnIcons";
+import MenuBtn from "./MenuBtn";
+import styled from "styled-components";
+
+function MainView() {
+    return (
+        <>
+            <Wrapper>
+                <ContentsBox>
+                    <ServiceDescription>
+                        <ServiceComment>
+                            당신의 조달 등록을 도와드립니다.
+                        </ServiceComment>
+                        무엇을 찾고 계신가요?
+                    </ServiceDescription>
+                    <BtnBox>
+                        <Link to={'/chat'} style={{ textDecoration: "none"}}>
+                            <BtnItem>
+                                {BtnIcons.map((item) =>
+                                    <MenuBtn icon={item.icon()} text={item.text}/>
+                                )}
+                            </BtnItem>
+                        </Link>
+
+                    </BtnBox>
+                </ContentsBox>
+            </Wrapper>
+        </>
+
+    );
+}
+
+export default MainView;
+
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: auto;
+`
+
+const ContentsBox = styled.div`
+    @media (max-width:${({theme}) => theme.mobile} ) {
+        margin: 100px 0;
+    }
+`
+
+const ServiceDescription = styled.div`
+    font-family: Inter;
+    text-align: center;
+    font-size: 30px;
+    font-weight: bold;
+    padding: 30px;
+    color: ${({theme}) => theme.colors.logoFont};
+    @media (max-width:${({theme}) => theme.mobile} ) {
+        margin-bottom: 20px;
+    }
+`
+
+const ServiceComment=styled.div`
+    font-size: 17px;
+    color: ${({theme}) => theme.colors.description};
+    padding: 2px;
+    font-weight: normal;
+    
+`
+
+const BtnBox = styled.div`
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    margin-bottom: 80px;
+    
+`
+const BtnItem =styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px; 
+    width: fit-content; 
+`
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,31 +1,11 @@
-import styled from "styled-components";
-import MenuBtn from "../../components/main/MenuBtn";
-import {BtnIcons} from "../../components/main/BtnIcons";
-import {Link} from "react-router-dom";
+import CookieModal from "../../components/modal/CookieModal";
+import MainView from "../../components/main/MainView";
 
 function MainPage() {
     return (
         <>
-            <Wrapper>
-                <ContentsBox>
-                    <ServiceDescription>
-                        <ServiceComment>
-                            당신의 조달 등록을 도와드립니다.
-                        </ServiceComment>
-                        무엇을 찾고 계신가요?
-                       </ServiceDescription>
-                    <BtnBox>
-                        <Link to={'/chat'} style={{ textDecoration: "none"}}>
-                            <BtnItem>
-                                {BtnIcons.map((item) =>
-                                    <MenuBtn icon={item.icon()} text={item.text}/>
-                                )}
-                            </BtnItem>
-                        </Link>
-
-                    </BtnBox>
-                </ContentsBox>
-            </Wrapper>
+            <CookieModal/>
+            {/*<MainView/>*/}
 
         </>
 
@@ -33,50 +13,3 @@ function MainPage() {
 }
 
 export default MainPage;
-
-const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    margin: auto;
-   
-`
-
-const ContentsBox = styled.div`
-    @media (max-width:${({theme}) => theme.mobile} ) {
-        margin: 100px 0;
-    }
-`
-
-const ServiceDescription = styled.div`
-    font-family: Inter;
-    text-align: center;
-    font-size: 30px;
-    font-weight: bold;
-    padding: 30px;
-    color: ${({theme}) => theme.colors.logoFont};
-    @media (max-width:${({theme}) => theme.mobile} ) {
-        margin-bottom: 20px;
-    }
-`
-
-const ServiceComment=styled.div`
-    font-size: 17px;
-    color: ${({theme}) => theme.colors.description};
-    padding: 2px;
-    font-weight: normal;
-    
-`
-
-const BtnBox = styled.div`
-    display: flex;
-    justify-content: center;
-    text-align: center;
-    margin-bottom: 80px;
-    
-`
-const BtnItem =styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 10px; 
-    width: fit-content; 
-`
