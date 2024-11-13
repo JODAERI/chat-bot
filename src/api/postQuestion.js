1920e239ae2cd93a821a49c5ae4e3fea7cfd93e6
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-13 23:51:51 +0900

    feat: 유저 아이디에 따라서 api 연동 request body 변경

--- src/api/postQuestion.js
+++ src/api/postQuestion.js
@@ -1,13 +1,30 @@
 import { axiosInstance } from "./instance";
+import Cookies from "js-cookie";
 
-export default async function postQuestion({ userId, question, isFirst }) {
+
+export default async function postQuestion(question,isFirst) {
   try {
-    const response = await axiosInstance.post("/api/question", {
-      user_id: userId,
-      question: question,
-      is_first: isFirst,
-    });
-    return response;
+    if (isFirst===true){
+      const response = await axiosInstance.post("/api/question", {
+            question: question,
+            is_first: isFirst
+          }
+      );
+      Cookies.set('user_id',response.data.userId)
+      return response.data;
+    }
+
+    if (isFirst===false){
+      const userId = Cookies.get('user_id')
+      const response = await axiosInstance.post("/api/question", {
+        user_id:userId,
+        question: question,
+        is_first: isFirst
+          }
+      );
+      return response.data;
+    }
+
   } catch (e) {
     console.log(e);
   }
