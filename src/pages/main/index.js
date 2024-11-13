193d5b3ff2a9558b777f35771c82f1e259d732d3
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-11-14 00:19:35 +0900

    docs: 전역변수 사용 재정비

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,13 +1,13 @@
 import CookieModal from "../../components/modal/CookieModal";
 import MainView from "../../components/main/MainView"
 
-function MainPage( { isCookieAccepted, onChangeCookies, setCookie,  isFirst,onChangeIsFirst }) {
+function MainPage( { isCookieAccepted, onChangeCookies,onChangeIsFirst,setQuickQuestion }) {
     return (
         <>
             {isCookieAccepted ?(
-                <MainView isFirst={isFirst} onChangeIsFirst={onChangeIsFirst} />
+                <MainView onChangeIsFirst={onChangeIsFirst} setQuickQuestion={setQuickQuestion}/>
             ):(
-                <CookieModal onChangeCookiePage={onChangeCookies} setCookie={setCookie}/>
+                <CookieModal onChangeCookiePage={onChangeCookies} />
             )
             }
         </>
